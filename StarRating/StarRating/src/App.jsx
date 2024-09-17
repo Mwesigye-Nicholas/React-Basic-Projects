51ca04964452c13175c90ab923a125d0b132b5f4
Mwesigye_Nicholas <nicholastoshack12345@gmail.com>
2024-09-16 20:03:22 -0600

    react js project

---
+++ StarRating/StarRating/src/App.jsx
@@ -0,0 +1,11 @@
+import StarRatings from "./components/StarRatings.jsx"
+function App() {
+
+  return (
+    <>
+      <StarRatings stars={10} />
+    </>
+  )
+}
+
+export default App
---
+++ StarRating/StarRating/src/components/StarRatings.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./styles.css";
+
+function StarRatings({ stars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+    console.log(rating)
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+  function handelMouseLeave() {
+    setHover(rating);
+  }
+  return (
+    <div className="star-rating">
+      {[...Array(stars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            className={index <= (hover || rating) ? "active" : " inactive"}
+            key={index}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handelMouseLeave ()}
+            size={60}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default StarRatings;
